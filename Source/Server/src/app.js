f2f324a706a1e85b4e13b63176fecdc7d02a3e9b
Akalanka <akalankaperera128@gmail.com>
2022-09-25 20:36:24 +0530

    Disabled cron job

--- Source/Server/src/app.js
+++ Source/Server/src/app.js
@@ -66,5 +66,5 @@ const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Watchnode server successfully started on port ${port}`)
-  initNotificationBroadcastCronJob()
+  // initNotificationBroadcastCronJob()
 })
